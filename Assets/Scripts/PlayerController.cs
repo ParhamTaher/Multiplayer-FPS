e8d120bbb57f76a7a681f5afe35519e2f79e26bd
ParhamTaher <parham.taher@gmail.com>
2018-08-16 20:06:43 -0400

    Initial commit and player control setup

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSpeed = 3f;
+    private PlayerMotor motor;
+
+	// Use this for initialization
+	void Start () {
+
+        motor = GetComponent<PlayerMotor>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 movHorizontal = transform.right * xMov;
+        Vector3 movVertical = transform.forward * zMov;
+
+        Vector3 vel = (movHorizontal + movVertical).normalized * speed;
+
+        motor.Move(vel);
+
+        // Turning left and right
+        float yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 rot = new Vector3(0f, yRot, 0f) * lookSpeed;
+
+        motor.Rotate(rot);
+
+        // Camera Rotation up and down
+        float xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 camerRot = new Vector3(xRot, 0f, 0f) * lookSpeed;
+
+        motor.RotateCamera(camerRot);
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+    [SerializeField]
+    private Camera cam;
+    private Vector3 vel = Vector3.zero;
+    private Vector3 rot = Vector3.zero;
+    private Vector3 camRot = Vector3.zero;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Move(Vector3 _vel)
+    {
+
+        Debug.Log("here!");
+        vel = _vel;
+
+    }
+
+    public void Rotate(Vector3 _rot)
+    {
+
+        rot = _rot;
+
+    }
+
+    public void RotateCamera(Vector3 _camRot)
+    {
+
+        camRot = _camRot;
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        PerformMovement();
+        PerformRotation();
+
+    }
+
+    public void PerformMovement()
+    {
+
+        if(vel != Vector3.zero)
+        {
+
+            rb.MovePosition(transform.position + vel * Time.fixedDeltaTime);
+
+        }
+
+    }
+
+    public void PerformRotation()
+    {
+        rb.MoveRotation(transform.rotation * Quaternion.Euler(rot));
+
+        if (cam != null)
+        {
+
+            cam.transform.Rotate(-camRot);
+
+        }
+
+    }
+
+
+}
